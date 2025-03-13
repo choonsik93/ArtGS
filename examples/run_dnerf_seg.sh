df2a36cc0f793fda19e4cfb702aaa22b75ae41a4
choonsik93 <cjg429@gmail.com>
2025-03-13 22:22:44 +0900

    param changed

--- examples/run_dnerf_seg.sh
+++ examples/run_dnerf_seg.sh
@@ -1,2 +1,3 @@
 python train.py -s data/dnerf/$1 --expname "dnerf/$1" --configs arguments/dnerf/$1.py &&
 python train_seg.py -s data/dnerf/$1 --expname "dnerf/$1" --configs arguments/dnerf/$1.py --decomp_configs config/dnerf/$1.yaml
+#python render_seg.py --model_path "output/dnerf/$1/" --skip_train --configs arguments/dnerf/$1.py
